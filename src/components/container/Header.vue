71ca2814639975c0a589b8936c38efe26998e650
1220x <1960708821@qq.com>
2022-01-08 18:22:53 +0800

    feat: ElHeader组件开发；全局注册组件

---
+++ src/components/container/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header
+        calss="el-header"
+        :style="{ height }"
+    >
+    </header>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'ElHeader'
+}
+</script>
+
+<script setup lang="ts">
+
+import { withDefaults, defineProps } from 'vue';
+
+interface Props {
+    height?: string
+}
+
+// withDefaults 用来设置属性的默认值
+withDefaults(defineProps<Props>(), {
+    height: '60px'
+})
+</script>
+
+<style lang="scss">
+@import '../../styles/mixin.scss';
+@include b(header) {
+    // padding: $--header-padding;
+    padding: 10px;
+    box-sizing: border-box;
+    flex-shrink: 0;
+}
+</style>
+
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Container from './components/container/Container.vue';
+import Header from './components/container/Header.vue';
 
-createApp(App).mount('#app')
+import './styles/index.scss';
+
+createApp(App)
+    .component('el-container', Container)
+    .component('el-header', Header)
+    .mount('#app')
